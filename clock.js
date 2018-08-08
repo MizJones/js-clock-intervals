10afbafaf4baa848b2b2250b9cef78f12c33b047
Gwendolyn Jones <gmjones206@gmail.com>
2018-08-08 10:06:16 -0700

    final version

--- clock.js
+++ clock.js
@@ -1,23 +1,66 @@
 
-var degreesMinSec = 0;
-var deg = 7
-var i = 0;
-var secHand = document.getElementById("second")
-do 
-{
-alert(degreesMinSec + "this is degreesMinSec at top of loop");
-secHand.style.transform = "rotate(7deg)";
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("dom is here");
+});
+
+var secHand = document.getElementById('second');
+var minHand = document.getElementById('minute');
+var hrHand = document.getElementById('hour');
+var sec;
+var min;
+var hr;
+var degS = 0;
+var degM = 0;
+var degH = 0;
+
+
+
+
+// setInterval(seconds, 1000)
+
+// Second Hand Motion, 6 decgrees per second,
+function seconds() {
+    console.log("secHand = " + secHand);
+    degS = (degS + 6);
+    secHand.style.transform = ('rotate(' + degS + 'deg)');
+    console.log('rotate(' + degS + 'deg)');
+}
+setInterval(seconds, 1000);
+setInterval(minutes, 60000);
+setInterval(hours, 3600000);
+
+//Minute Hand Motion, 6 degrees per minute,
+
+function minutes() {
+    console.log("minHand = " + minHand);
+	degM = (degM + 6);
+    minHand.style.transform = ('rotate(' + degM + 'deg)');
+    console.log('rotate(' + degM + 'deg)');
+}
+
+
+// Hour Hand motion, 30 degrees per hour
+function hours() {
+    console.log("hrHand =" + secHand);
+	degH = (degH + 30);
+    hrHand.style.transform = ('rotate(' + degH + 'deg)');
+    console.log('rotate(' + degH + 'deg)');
+}
+// do 
+// {
+//alert(degreesMinSec + "this is degreesMinSec at top of loop");
+//secHand.style.transform = "rotate(7deg)";
 
 //second.style.transform = "rotate(degreesMinSec + 7);
-degreesMinSec = degreesMinSec + 7;
+//degreesMinSec = degreesMinSec + 7;
 //document.getElementById("second");
 //el.style.transform = "rotate(" + degreesMinSec + "degreesMinSec)";
-alert(degreesMinSec + "this is degreesMinSec in bottom of loop");
-i++;
+//alert(degreesMinSec + "this is degreesMinSec in bottom of loop");
+/*i++;
 //alert(i);
 }
 while (i < 4)
-
+*/
 
 /*
 
